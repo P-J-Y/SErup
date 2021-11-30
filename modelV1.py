e579ea5e7a273a42c3a645e10401219a80d663ce
彭镜宇 <2101110617@pku.edu.cn>
2021-11-30 08:08:43 +0800

    自动调参

--- modelV1.py
+++ modelV1.py
@@ -61,6 +61,8 @@ def pre_generator(xdata,batch_size):
 ###################### Training #####################
 
 if __name__ == "__main__":
+
+#################################### load dataset ################################################
     #xtrain_orig, ytrain, xtest_orig, ytest, classes = V1_utils.load_dataset(filename='data/data24hr_1hr/data24hr_1hr.h5')
     xtrain_orig, ytrain, xtest_orig, ytest, classes = V1_utils.load_dataset(filename='data/data60to30/data60to30.h5')
     #xtrain_orig,ytrain,classes = V1_utils.load_dataset_tot('data/data60/data60tot.h5')
@@ -81,55 +83,124 @@ if __name__ == "__main__":
     #model_v1 = V1_utils.modelResnet(X_train.shape[1:])
     #model_v1 = V1_utils.modelVgg19(X_train.shape[1:])
 
-    lambda_l2 = 0.04
-    lr = 0.00005
+############################################# modelV1 ######################################
+    # lambda_l2 = 0.04
+    # lr = 0.00005
+    #
+    # model_v1 = V1_utils.modelV1(X_train.shape[1:],lambda_l2=lambda_l2)
+    # # plot_model(model_v1, to_file='model.png')
+    # # SVG(model_to_dot(model_v1).create(prog='dot', format='svg'))
+    # # 编译模型（在锁层以后操作）
+    # opt = tensorflow.keras.optimizers.Adam(lr=lr)
+    #
+    # model_v1.compile(loss="binary_crossentropy", metrics=['accuracy'],optimizer=opt)
+    # # 训练模型
+    # batch_size = 16
+    # steps_per_epoch = (np.shape(X_train)[0] + batch_size - 1) // batch_size
+    # metrics = V1_utils.Metrics(test_data=(X_test[::10], Y_test[::10]),train_data=(X_train[::100],Y_train[::100]))
+    #
+    # from sklearn.utils import class_weight
+    # import pandas as pd
+    # class_weight = class_weight.compute_class_weight(class_weight='balanced',
+    #                                                  classes=classes,
+    #                                                  y=Y_train[:,0])
+    # cw = dict(enumerate(class_weight))
+    #
+    # history=model_v1.fit_generator(generator=data_generator(X_train, Y_train, batch_size),
+    #                        steps_per_epoch=steps_per_epoch,
+    #                        epochs=30,
+    #                        verbose=1,
+    #                        validation_data=(X_test[::20], Y_test[::20]),
+    #                        callbacks=[metrics],
+    #                        class_weight=cw,
+    #                        )
+    # plt.figure()
+    # plt.plot(history.history['loss'], 'b', label='Training loss')
+    # plt.plot(history.history['val_loss'], 'r', label='Validation val_loss')
+    # plt.title('Traing and Validation loss')
+    # plt.legend()
+    # plt.savefig('figure/log/loss.jpg')
+    #
+    # #model_v1.fit(X_train, Y_train, epochs=10, batch_size=8,validation_data=(X_test[::2],Y_test[::2]),callbacks = [metrics],)
+    # # 评估模型
+    # batch_size_test = 4
+    # preds = model_v1.evaluate_generator(generator=val_generator(X_test, Y_test, batch_size_test),
+    #                                     verbose=1)
+    # #警告，不用管 UserWarning: `Model.evaluate_generator` is deprecated and will be removed in a future version.Please use `Model.evaluate`, which supports generators.
+    #
+    # # preds = model_v1.evaluate(X_test, Y_test, batch_size=8, verbose=1, sample_weight=None)
+    # print("误差值 = " + str(preds[0]))
+    # print("准确度 = " + str(preds[1]))
+    # cvres = model_v1.predict_generator(pre_generator(X_test,4),verbose=1)
+    # cvf1s,cache = V1_utils.fmeasure(Y_test,cvres)
+    # p,r = cache
+    # print("f1 = {}, precision = {}, recall = {}".format(cvf1s,p,r))
+    # print("hhh")
 
-    model_v1 = V1_utils.modelV1(X_train.shape[1:],lambda_l2=lambda_l2)
-    plot_model(model_v1, to_file='model.png')
-    SVG(model_to_dot(model_v1).create(prog='dot', format='svg'))
-    # 编译模型（在锁层以后操作）
-    opt = tensorflow.keras.optimizers.Adam(lr=lr)
 
-    model_v1.compile(loss="binary_crossentropy", metrics=['accuracy'],optimizer=opt)
-    # 训练模型
-    batch_size = 16
-    steps_per_epoch = (np.shape(X_train)[0] + batch_size - 1) // batch_size
-    metrics = V1_utils.Metrics(test_data=(X_test[::10], Y_test[::10]),train_data=(X_train[::100],Y_train[::100]))
+################################# hyperopt model #####################################
+from hyperopt import hp, STATUS_OK, Trials, fmin, tpe
+
+#需要的参数
+#lr 1e-5 to 1e-1
+#lambda_l2 1e-5 to 1e-1
+#batch_size 2 to 16
+
+space = {
+    'lr':hp.loguniform('lr',-10,-1),
+    'lambda_l2':hp.loguniform('lambda_l2',-10,-1),
+    'batch_size':hp.choice('batch_size',[2,4,8,16])
+}
+
+f1 = 0
+def trainAmodel(params):
+    global X_train,X_test,Y_test,Y_train
+    global f1
+    print('Params testing: ', params)
+    model_v1 = V1_utils.modelV1(X_train.shape[1:],params)
+    opt = tensorflow.keras.optimizers.Adam(lr=params['lr'])
+    model_v1.compile(loss="binary_crossentropy", metrics=['accuracy'], optimizer=opt)
+    steps_per_epoch = (np.shape(X_train)[0] + params['batch_size'] - 1) // params['batch_size']
+    metrics = V1_utils.Metrics(test_data=(X_test[::10], Y_test[::10]), train_data=(X_train[::100], Y_train[::100]))
 
     from sklearn.utils import class_weight
     import pandas as pd
     class_weight = class_weight.compute_class_weight(class_weight='balanced',
                                                      classes=classes,
-                                                     y=Y_train[:,0])
+                                                     y=Y_train[:, 0])
     cw = dict(enumerate(class_weight))
 
-    history=model_v1.fit_generator(generator=data_generator(X_train, Y_train, batch_size),
-                           steps_per_epoch=steps_per_epoch,
-                           epochs=30,
-                           verbose=1,
-                           validation_data=(X_test[::20], Y_test[::20]),
-                           callbacks=[metrics],
-                           class_weight=cw,
-                           )
-    plt.figure()
-    plt.plot(history.history['loss'], 'b', label='Training loss')
-    plt.plot(history.history['val_loss'], 'r', label='Validation val_loss')
-    plt.title('Traing and Validation loss')
-    plt.legend()
-    plt.savefig('figure/log/loss.jpg')
-
-    #model_v1.fit(X_train, Y_train, epochs=10, batch_size=8,validation_data=(X_test[::2],Y_test[::2]),callbacks = [metrics],)
+    history = model_v1.fit_generator(generator=data_generator(X_train, Y_train, params['batch_size']),
+                                     steps_per_epoch=steps_per_epoch,
+                                     epochs=30,
+                                     verbose=1,
+                                     validation_data=(X_test[::20], Y_test[::20]),
+                                     callbacks=[metrics],
+                                     class_weight=cw,
+                                     )
     # 评估模型
     batch_size_test = 4
     preds = model_v1.evaluate_generator(generator=val_generator(X_test, Y_test, batch_size_test),
                                         verbose=1)
-    #警告，不用管 UserWarning: `Model.evaluate_generator` is deprecated and will be removed in a future version.Please use `Model.evaluate`, which supports generators.
-
-    # preds = model_v1.evaluate(X_test, Y_test, batch_size=8, verbose=1, sample_weight=None)
     print("误差值 = " + str(preds[0]))
     print("准确度 = " + str(preds[1]))
-    cvres = model_v1.predict_generator(pre_generator(X_test,4),verbose=1)
-    cvf1s,cache = V1_utils.fmeasure(Y_test,cvres)
-    p,r = cache
-    print("f1 = {}, precision = {}, recall = {}".format(cvf1s,p,r))
-    print("hhh")
+    cvres = model_v1.predict_generator(pre_generator(X_test, 4), verbose=1)
+    cvf1s, cache = V1_utils.fmeasure(Y_test, cvres)
+    p, r = cache
+    print("f1 = {}, precision = {}, recall = {}".format(cvf1s, p, r))
+    if cvf1s>f1:
+        f1 = cvf1s
+        model_v1.save('model/v1/model_v1_1.h5')
+    print(f1)
+    return {
+        'loss':-cvf1s,
+        'status':STATUS_OK
+    }
+
+trials = Trials()
+best = fmin(trainAmodel, space, algo=tpe.suggest, max_evals=100, trials=trials)
+print('best')
+print(best)
+for trial in trials.trials[:100]:
+    print(trial)
+
