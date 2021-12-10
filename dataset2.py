6385d0daa453f5a17949ef1c0d17aec99c685d3e
P-J-Y <2101110617@pku.edu.cn>
2021-12-11 01:15:32 +0800

    Update dataset2.py

--- dataset2.py
+++ dataset2.py
@@ -510,21 +510,6 @@ def negativeSamping(fileName='data/data2/0/testneg.h5',
     file.create_dataset('DATA', data=np.array(DATA))
     file.close()
 
-for i in range(0,42):
-# for i in range(13, 14):
-    print('i={}'.format(i))
-    positiveSampling(fileName='data/data2/1/pos{}.h5'.format(i),
-                     #fileName='data/data2/1/pos{}.h5'.format('test'),
-                     freq='30min',
-                     observatorys=("SDO", "SDO", "SDO", "SDO", "SDO", "SDO", "SDO",),
-                     instruments=("HMI", "AIA", "AIA", "AIA", "AIA", "AIA", "AIA"),
-                     measurements=("magnetogram","94", "171", "193", "211", "304", '1700'),
-                     imgSize=256,
-                     i1=25 * i,
-                     i2=25 * (i + 1),
-                     #i2=50*i+1,
-                     )
-
 # negativeSamping(  # fileName='data/data2/0/testneg.h5',
 #         fileName='data/data2/0/neg{}.h5'.format('test'),
 #         observatorys=("SDO", "SDO", "SDO", "SDO", "SDO", "SDO", "SDO",),
@@ -544,48 +529,77 @@ def keep_connect(url="https://baidu.com"):
                 connected = True
                 return True
             else:
-                print("未连接，等待10s")
-                time.sleep(10)
+                print("未连接，等待5s")
+                time.sleep(5)
                 continue
         except:
-            print("未连接，等待10s")
-            time.sleep(10)
+            print("未连接，等待5s")
+            time.sleep(5)
             continue
         # finally:
         #     "未连接，等待10s"
         #     time.sleep(10)
 
 
+i = 50
+print('i={}'.format(i))
+done = False
+while not done:
+    try:
+        negativeSamping(fileName='data/data2/0/testneg.h5',
+                        # fileName='data/data2/0/neg{}.h5'.format(i),
+                        observatorys=("SDO", "SDO", "SDO", "SDO", "SDO", "SDO", "SDO",),
+                        instruments=("HMI", "AIA", "AIA", "AIA", "AIA", "AIA", "AIA"),
+                        measurements=("magnetogram", "94", "171", "193", "211", "304", '1700'),
+                        i1=25 * i,
+                        # i2=100*(i+1),
+                        i2=25 * (i + 1))
+        done = True
+    except (RuntimeError, IOError):
+        print("RuntimeError/IOError 检查网络连接是否正常")
+        intc = keep_connect()
+        print("网络连接正常 检查Helioviewer网站连接是否正常")
+        url = 'https://helioviewer.org'
+        hvc = keep_connect(url=url)
+        print('连接正常，重新运行程序')
+        dirname = 'C:/Users/pjy/sunpy/data'
+        # 把最近下载的文件删除（因为这个文件很可能是坏的）
+        dir_list = os.listdir(dirname)
+        if dir_list:
+            dir_list = sorted(dir_list,
+                              key=lambda x: os.path.getctime(os.path.join(dirname, x)))
+            os.remove(dirname + '/' + dir_list[-1])
+        continue
 
-for i in range(48, 57):
-    print('i={}'.format(i))
-    done = False
-    while not done:
-        try:
-            negativeSamping(  # fileName='data/data2/0/testneg.h5',
-                fileName='data/data2/0/neg{}.h5'.format(i),
-                observatorys=("SDO", "SDO", "SDO", "SDO", "SDO", "SDO", "SDO",),
-                instruments=("HMI", "AIA", "AIA", "AIA", "AIA", "AIA", "AIA"),
-                measurements=("magnetogram", "94", "171", "193", "211", "304", '1700'),
-                i1=25 * i,
-                # i2=100*(i+1),
-                i2=25 * (i + 1))
-            done = True
-        except (RuntimeError,IOError):
-            print("RuntimeError/IOError 检查网络连接是否正常")
-            intc = keep_connect()
-            print("网络连接正常 检查Helioviewer网站连接是否正常")
-            url = 'https://helioviewer.org'
-            hvc = keep_connect(url=url)
-            print('连接正常，重新运行程序')
-            dirname = 'C:/Users/pjy/sunpy/data'
-            # 把最近下载的文件删除（因为这个文件很可能是坏的）
-            dir_list = os.listdir(dirname)
-            if dir_list:
-                dir_list = sorted(dir_list,
-                                  key=lambda x: os.path.getctime(os.path.join(dirname, x)))
-                os.remove(dirname + '/' + dir_list[-1])
-            continue
+# for i in range(48, 57):
+#     print('i={}'.format(i))
+#     done = False
+#     while not done:
+#         try:
+#             negativeSamping(  # fileName='data/data2/0/testneg.h5',
+#                 fileName='data/data2/0/neg{}.h5'.format(i),
+#                 observatorys=("SDO", "SDO", "SDO", "SDO", "SDO", "SDO", "SDO",),
+#                 instruments=("HMI", "AIA", "AIA", "AIA", "AIA", "AIA", "AIA"),
+#                 measurements=("magnetogram", "94", "171", "193", "211", "304", '1700'),
+#                 i1=25 * i,
+#                 # i2=100*(i+1),
+#                 i2=25 * (i + 1))
+#             done = True
+#         except (RuntimeError,IOError):
+#             print("RuntimeError/IOError 检查网络连接是否正常")
+#             intc = keep_connect()
+#             print("网络连接正常 检查Helioviewer网站连接是否正常")
+#             url = 'https://helioviewer.org'
+#             hvc = keep_connect(url=url)
+#             print('连接正常，重新运行程序')
+#             dirname = 'C:/Users/pjy/sunpy/data'
+#             # 把最近下载的文件删除（因为这个文件很可能是坏的）
+#             dir_list = os.listdir(dirname)
+#             if dir_list:
+#                 dir_list = sorted(dir_list,
+#                                   key=lambda x: os.path.getctime(os.path.join(dirname, x)))
+#                 os.remove(dirname + '/' + dir_list[-1])
+#             continue
 
 
 
